b5f939ec01921b8fa6d65388cb4a5a6f2944ca17
mtvProof <mtvproofsot@gmail.com>
2026-05-14 20:41:43 -0300

    Debug: Add logging to check config structure in ENV_ONLY mode

--- src/utils/configLoader.js
+++ src/utils/configLoader.js
@@ -104,6 +104,8 @@ function loadGlobalConfig() {
     if (ENV_ONLY_MODE) {
         config = buildConfigFromEnv('UR_CONFIG');
         console.log('[ConfigLoader] Running in ENV_ONLY mode - loaded config from environment variables');
+        console.log('[ConfigLoader] Global config keys:', Object.keys(config));
+        console.log('[ConfigLoader] PLAYER_PROFILER exists:', !!config.PLAYER_PROFILER);
     } else {
         // Legacy mode: read from JSON file and allow env overrides
         if (fs.existsSync(CONFIG_PATH)) {
